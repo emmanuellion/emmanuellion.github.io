73776790a1073740a062a875d689ee4c2cc5bcfc
Mabule <55059322+Mabule@users.noreply.github.com>
2020-08-16 17:11:38 +0200

    v15.25

--- snake/js/main.js
+++ snake/js/main.js
@@ -17,6 +17,14 @@ var yA;
 var xB;
 var yB; 
 var path;
+var closedList = [];
+var openList = [];
+xA = Math.floor(Math.random() * (nb_case - 1) + 1) - 1;
+yA = Math.floor(Math.random() * (nb_case - 1) + 1) - 1;
+xB = Math.floor(Math.random() * (nb_case - 1) + 1) - 1;
+yB = Math.floor(Math.random() * (nb_case - 1) + 1) - 1;
+var start;
+var end;
 
 for (i = 0; i < nb_case; i++){
  	ctx.beginPath();
@@ -35,19 +43,19 @@ for (i = 0; i < nb_case; i++){
 function Cells(i,j){
 	this.i = i;
 	this.j = j;
-	this.show = function (color){
-		ctx.fillStyle = color;
-		ctx.fillRect(this.i*case_size,this.j*case_size,case_size,case_size);
-	}
 	this.wall = false;
 	this.f = 0;
 	this.g = 0;
 	this.h = 0;
+	this.show = function (color){
+		ctx.fillStyle = color;
+		ctx.fillRect(this.i*case_size,this.j*case_size,case_size,case_size);
+	}
 	this.neighbors = [];
 	this.previous = undefined;
 	this.addNeighbors = function(grid) {
-		var i = this.i;
-		var j = this.j;
+		i = this.i;
+		j = this.j;
 		if(i < nb_case - 1){
 			this.neighbors.push(grid[i+1][j]);
 		}
@@ -64,7 +72,7 @@ function Cells(i,j){
 }
 
 function RemoveFromArray(arr, elt){
-	for (var i = arr.length - 1; i >= 0; i--) {
+	for (i = arr.length - 1; i >= 0; i--) {
 		if (arr[i] == elt){
 			arr.splice(i, 1);
 		}
@@ -101,25 +109,20 @@ for (i = 0; i < nb_case; i++){
 	}
 }
 
-var closedList = [];
-var openList = [];
-
-xA = Math.floor(Math.random() * (nb_case - 0) + 0) - 1;
-yA = Math.floor(Math.random() * (nb_case - 0) + 0) - 1;
-xB = Math.floor(Math.random() * (nb_case - 0) + 0) - 1;
-yB = Math.floor(Math.random() * (nb_case - 0) + 0) - 1;
-var start = grid[xA][yA];
-grid[xA][yA].show("green");
-var end = grid[xB][yB];
-grid[xB][yB].show("red");
-
+start = grid[xA][yA];
+end = grid[xB][yB];
 console.log("x : " + end.i + "\ny : " + end.j);
+grid[xA][yA].show("green");
+grid[xB][yB].show("red");
+start.wall = false;
+end.wall = false;
+
 openList.push(start);
 
 function A(){
 	if (openList.length > 0){
 		var lowestIndex = 0;
-		for (var i = 0; i < openList.length; i++){
+		for (i = 0; i < openList.length; i++){
 			if(openList[i].f < openList[lowestIndex].f){
 				lowestIndex = i;
 			}
@@ -127,8 +130,6 @@ function A(){
 		var current = openList[lowestIndex];
 		if (current === end){
 			console.log("Finis !");
-			console.log(current);
-			console.log(end);
 			path = [];
 			var temp = current;
 			path.push(temp);
@@ -136,21 +137,27 @@ function A(){
 				path.push(temp.previous);
 				temp = temp.previous;
 			}
-			for (var i = 0; i < path.length; i++){
-				grid[path[i].i][path[i].j].show("orange");
+			for (i = 0; i < path.length; i++){
+				if (i==0){
+					grid[path[i].i][path[i].j].show("red");
+				}else if(i == path.length-1){
+					grid[path[i].i][path[i].j].show("green");
+				}else{
+					grid[path[i].i][path[i].j].show("orange");
+				}
 			}
-			return;
+			stop();
+			openList = 0;
 		}
 		RemoveFromArray(openList, current);
 		closedList.push(current);
 		var neighbors = current.neighbors;
-		for (var i = 0; i < neighbors.length; i++){
+		for (i = 0; i < neighbors.length; i++){
 			var neighbor = neighbors[i];
-			if (!closedList.includes(neighbor)){
+			if (!closedList.includes(neighbor) && !neighbor.wall){
 				var tempG = current.g + 1;
-				grid[current.i][current.j].show("blue");
-				if (openList.includes(neighbor) || neighbor.wall == true){
-					if(tempG < neighbor.g){
+				if (openList.includes(neighbor)){
+					if(tempG < neighbor.g ){
 						neighbor.g = tempG;
 					}
 				}else{
@@ -163,8 +170,31 @@ function A(){
 			}
 		}
 	}else{
-	
+		console.log("Pas d'issues");
+		return 0;
 	}	
+	for(i = 0; i < openList.length; i++){
+		if (openList[i].i==xA && openList[i].j==yA){
+			grid[openList[i].i][openList[i].j].show("green");
+		}else if(openList[i].i==xB && openList[i].j==yB){
+			grid[openList[i].i][openList[i].j].show("red");
+		}else{
+			grid[openList[i].i][openList[i].j].show("blue");
+		}
+	}
+	for(i = 0; i < closedList.length; i++){
+		if (closedList[i].i==xA && closedList[i].j==yA){
+			grid[closedList[i].i][closedList[i].j].show("green");
+		}else if(closedList[i].i==xB && closedList[i].j==yB){
+			grid[closedList[i].i][closedList[i].j].show("red");
+		}else{
+			grid[closedList[i].i][closedList[i].j].show("grey");
+		}
+	}
 }
 
-var interval = setInterval(A,100);
+var interval = setInterval(A,1);
+
+function stop(){
+	return 0;
+}
