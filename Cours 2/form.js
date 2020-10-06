05249ee9e9d0f9bf14dd8aa69b0932b68d84fecf
Emmanuel <drakenunity@gmail.com>
2020-10-06 14:54:03 +0200

    v26.0

---
+++ Cours 2/form.js
@@ -0,0 +1,59 @@
+const i1 = document.getElementsByTagName('input')[0];
+const i2 = document.getElementsByTagName('input')[1];
+const i3 = document.getElementsByTagName('input')[2];
+const i4 = document.getElementsByTagName('input')[3];
+
+const s1 = document.getElementsByTagName('span')[0];
+const s2 = document.getElementsByTagName('span')[1];
+const s3 = document.getElementsByTagName('span')[2];
+const s4 = document.getElementsByTagName('span')[3];
+
+function up(children){
+	children.style.transform = "translate(5px,0px)";
+	children.style.fontSize = "80%";
+	children.style.color = "#007BFF";
+}
+
+function down(obj, children){
+	if(obj.value == "" || obj.value == " "){
+		children.style.transform = "translate(10px,20px)";
+		children.style.fontSize = "90%";
+		children.style.color = "#6C757D";
+		obj.style.borderColor = "#6C757D";
+	}else{
+		children.style.color = "#007BFF";
+		obj.style.borderColor = "#007BFF";
+	}
+}
+
+i1.onfocus = function(){
+	up(s1);
+}
+
+i2.onfocus = function(){
+	up(s2);
+}
+
+i3.onfocus = function(){
+	up(s3);
+}
+
+i4.onfocus = function(){
+	up(s4);
+}
+
+i1.onblur = function(){
+	down(i1, s1);
+}
+
+i2.onblur = function(){
+	down(i2, s2);
+}
+
+i3.onblur = function(){
+	down(i3, s3);
+}
+
+i4.onblur = function(){
+	down(i4, s4);
+}
