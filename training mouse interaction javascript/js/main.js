96b78f9f6a9dba164a1e53b728aab480a6a99a33
LION Emmanuel <emmanuel.lion@etu.univ-amu.fr>
2021-03-20 02:08:34 +0100

    Ajout de tout ce qui touche au projet tut WebGl

---
+++ training mouse interaction javascript/js/main.js
@@ -0,0 +1,37 @@
+body = document.getElementsByTagName("body")[0];
+p = document.getElementsByTagName("p")[0];
+canvas = document.getElementsByTagName("canvas")[0];
+
+
+canvas.addEventListener('mousemove', logKey);
+
+function logKey(e) {
+  p.innerHTML = e.clientX + " " + e.clientY;
+}
+
+ball = canvas;
+ball.onmousedown = function(event) {
+
+  ball.style.position = 'absolute';
+  ball.style.zIndex = 1000;
+  document.body.append(ball);
+
+  function moveAt(pageX, pageY) {
+    ball.style.left = pageX - ball.offsetWidth / 2 + 'px';
+    ball.style.top = pageY - ball.offsetHeight / 2 + 'px';
+  }
+
+  moveAt(event.pageX, event.pageY);
+
+  function onMouseMove(event) {
+    moveAt(event.pageX, event.pageY);
+  }
+
+  document.addEventListener('mousemove', onMouseMove);
+
+  ball.onmouseup = function() {
+    document.removeEventListener('mousemove', onMouseMove);
+    ball.onmouseup = null;
+  };
+
+};
