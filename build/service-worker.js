dd7cca2d8f58812c2bf8242513a6e38bc0c9a86b
Emmanuel Lion <55059322+emmanuellion@users.noreply.github.com>
2023-06-10 14:57:41 +0200

    Add files via upload

---
+++ build/service-worker.js
@@ -0,0 +1,43 @@
+const cacheName = 'katana_cache';
+const filesToCache = [
+    '/',
+    '/index.html',
+    '/manifest.json',
+    '/katana.png',
+    // Ajoutez ici les autres ressources statiques de votre application
+];
+
+// eslint-disable-next-line no-restricted-globals
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(cacheName)
+            .then(cache => cache.addAll(filesToCache))
+            // eslint-disable-next-line no-restricted-globals
+            .then(() => self.skipWaiting())
+    );
+});
+
+// eslint-disable-next-line no-restricted-globals
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys()
+            .then(keyList => Promise.all(
+                // eslint-disable-next-line array-callback-return
+                keyList.map(key => {
+                    if (key !== cacheName) {
+                        return caches.delete(key);
+                    }
+                })
+            ))
+            // eslint-disable-next-line no-restricted-globals
+            .then(() => self.clients.claim())
+    );
+});
+
+// eslint-disable-next-line no-restricted-globals
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
+});
